8741a67eecb508aa14834ad6aae40788bf0da8be
didigiffi-dvfu <didgiffi-dvfu>
2026-05-08 19:31:40 +1000

    init

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+﻿const loginForm = document.getElementById('loginForm');
+
+if (loginForm) {
+    loginForm.onsubmit = function (e) {
+        e.preventDefault();
+
+        const user = document.getElementById('username').value;
+
+        alert(`Добро пожаловать, ${user} ✨`);
+        window.location.href = 'feed.html';
+    };
+}
+
+const regForm = document.getElementById('regForm');
+
+if (regForm) {
+    regForm.onsubmit = function (e) {
+        e.preventDefault();
+
+        alert('Аккаунт успешно создан ✨');
+        window.location.href = 'index.html';
+    };
+}
+
+function publishPost() {
+    const title = document.getElementById('newPostTitle').value;
+    const text = document.getElementById('newPostContent').value;
+
+    if (title.trim() === '' || text.trim() === '') {
+        alert('Заполни все поля ✨');
+        return;
+    }
+
+    alert(`Пост "${title}" опубликован ✨`);
+
+    document.getElementById('newPostTitle').value = '';
+    document.getElementById('newPostContent').value = '';
+}
+
+const likeButtons = document.querySelectorAll('.like-btn');
+
+likeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        button.innerHTML = '❤ Добавлено';
+    });
+});
+
+const saveButtons = document.querySelectorAll('.save-btn');
+
+saveButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert('Сохранено ✨');
+    });
+});
